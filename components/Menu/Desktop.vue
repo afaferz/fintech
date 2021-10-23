023ce09327c7b7ce92f83d2b5acd5c29c6942f14
afaferz <afa00.papa@gmail.com>
2021-10-23 18:19:22 -0300

    Update

---
+++ components/Menu/Desktop.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <v-app-bar color="#517ef7" height="100px">
+            <v-container fluid class="d-flex flex-row">
+                <v-row>
+                    <v-col cols="12">
+                        <NuxtLink to="">Seu Perfil</NuxtLink>
+                        <NuxtLink to="">Algo aqui</NuxtLink>
+                        <NuxtLink to="">Educação Financeira</NuxtLink>
+                        <NuxtLink to="">Simule o valor</NuxtLink>
+                        <NuxtLink to="">FAQ</NuxtLink>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-app-bar>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ layouts/principal-page.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-app>
+        <Desktop />
+        <v-main>
+            <v-container>
+                <Nuxt />
+            </v-container>
+        </v-main>
+    </v-app>
+</template>
+
+<script>
+import Desktop from "@/components/Menu/Desktop";
+import Mobile from "@/components/Menu/Mobile";
+export default {
+    layout: 'PrincipalPage',
+    components: {
+        Desktop,
+        Mobile
+    }
+};
+</script>
+
+<style>
+</style>
