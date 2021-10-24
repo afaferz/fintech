a72da5bb016e5290705d07eca9c735d89bc8298f
afaferz <afa00.papa@gmail.com>
2021-10-23 22:03:17 -0300

    Update

---
+++ pages/dudu.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-row justify="center" align="center">
+        Página pro Dudu fazer componente
+    </v-row>
+</template>
+
+<script>
+export default {
+    layout: 'principal-page',
+};
+</script>
+
+<style>
+</style>
