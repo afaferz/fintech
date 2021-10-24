22bc8068c61443d0e44ca04349e583aa1fa5bde4
afaferz <afa00.papa@gmail.com>
2021-10-24 15:01:31 -0300

    Update

--- pages/index.vue
+++ pages/index.vue
@@ -63,7 +63,7 @@ section {
         }
     }
     &[title="Educational"] {
-        background: linear-gradient(-55deg, #fefefe 42%, $primary 30%);
+        background: linear-gradient(-55deg, #fefefe 40%, $primary 30%);
         @media screen and (max-width: 600px) {
             background: linear-gradient(0deg, $primary 58%, #fefefe 55%);
         }
