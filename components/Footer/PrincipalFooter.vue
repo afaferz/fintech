52cb505bf9c80cea2575865cb2c4b40dab575aa8
afaferz <afa00.papa@gmail.com>
2021-10-24 08:33:58 -0300

    Update

--- components/Footer/PrincipalFooter.vue
+++ components/Footer/PrincipalFooter.vue
@@ -1,16 +1,41 @@
 <template>
-    <v-footer color="primary lighten-1" padless>
-        <v-row justify="center" no-gutters>
-            <v-btn
-                v-for="link in links"
-                :key="link"
-                color="white"
-                text
-                rounded
-                class="my-2"
-            >
-                {{ link }}
-            </v-btn>
+    <v-footer color="white lighten-1" elevation="6" class="pa-0">
+        <v-container>
+            <v-row justify="space-between" align="center" class="mt-4">
+                <v-col
+                    v-for="item in links"
+                    :key="item.title"
+                    cols="12"
+                    :md="12 / links.length"
+                    align-self="start"
+                    class="mx-4 mx-md-0"
+                >
+                    <p
+                        class="
+                            primary--text
+                            font-weight-black
+                            text-uppercase
+                            subheading
+                        "
+                    >
+                        {{ item.title }}
+                    </p>
+                    <p v-for="subItem in item.sublinks" :key="subItem.href">
+                        <NuxtLink
+                            :to="subItem.href"
+                            class="
+                                secondary--text
+                                text-decoration-none
+                                sublinks
+                            "
+                        >
+                            {{ subItem.title }}
+                        </NuxtLink>
+                    </p>
+                </v-col>
+            </v-row>
+        </v-container>
+        <v-row>
             <v-col
                 class="primary lighten-2 py-4 text-center white--text"
                 cols="12"
@@ -23,7 +48,79 @@
 <script>
 export default {
     data: () => ({
-        links: ["Home", "About Us", "Team", "Services", "Blog", "Contact Us"]
+        links: [
+            {
+                title: "Sobre nós",
+                sublinks: [
+                    { title: "Home", href: "" },
+                    { title: "Blog", href: "" },
+                    { title: "Quem somos", href: "" },
+                    { title: "Investimentos", href: "" }
+                ]
+            },
+            {
+                title: "Serviços",
+                sublinks: [
+                    { title: "Controle de finanças", href: "" },
+                    { title: "Gestão de risco", href: "" },
+                    { title: "Estudo de caso", href: "" },
+                    { title: "Mentorias individuais", href: "" },
+                    { title: "Simuladores", href: "" }
+                ]
+            },
+            {
+                title: "Aprenda conosco",
+                sublinks: [
+                    { title: "Mentorias", href: "" },
+                    { title: "Videos", href: "" },
+                    { title: "E-book", href: "" },
+                    { title: "Newsletter", href: "" }
+                ]
+            },
+            {
+                title: "Ajuda & Suporte",
+                sublinks: [
+                    { title: "Fale conosco", href: "" },
+                    { title: "FAQs", href: "" }
+                ]
+            },
+            {
+                title: "Fale Conosco",
+                sublinks: [
+                    { title: "Fale conosco", href: "" },
+                    { title: "FAQs", href: "" }
+                ]
+            },
+            {
+                title: "Termos & Privacidade",
+                sublinks: [
+                    { title: "Legal", href: "" },
+                    { title: "Legal", href: "" },
+                    { title: "Privacidade", href: "" },
+                    { title: "Segurança", href: "" }
+                ]
+            }
+        ]
     })
 };
 </script>
+
+<style lang="scss" scoped>
+.sublinks {
+    position: relative;
+    // transition: all 0.35s ease;
+    &::before {
+        transition: all 0.3s ease-in-out 0s;
+        background-color: #8eaeff;
+        bottom: -5px;
+        content: "";
+        display: inline-block;
+        height: 2px;
+        position: absolute;
+        width: 0%;
+    }
+    &:hover::before {
+        width: 95%;
+    }
+}
+</style>
